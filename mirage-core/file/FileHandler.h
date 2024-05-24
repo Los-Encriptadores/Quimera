00154f8f9cc36708087e89672ac0f58f820822b3
sirlolcat <sirlolcatX@pm.me>
2024-05-24 15:28:22 +0200

    feat: improve file handler
    Signed-off-by: sirlolcat <sirlolcatX@pm.me>

--- mirage-core/file/FileHandler.h
+++ mirage-core/file/FileHandler.h
@@ -1,15 +1,65 @@
-#ifndef FILEHANDLER_H
-#define FILEHANDLER_H
+/**
+ * @file FileHandler.h
+ * @brief This file contains the FileHandler class which is used for file operations.
+ * @author 0x5844
+ * @copyright GPLv3
+ * @date 2024
+*/
 
-#include <vector>
+#pragma once
 #include <string>
-#include <filesystem>
+#include <vector>
+#include <mutex>
+#include <sys/mman.h>
 
-class FileHandler {
-public:
-    static std::vector<uint8_t> readFile(const std::filesystem::path &filePath);
+/**
+ * @class FileHandler
+ * @brief The FileHandler class provides methods for loading and saving files.
+*/
+namespace file {
+    class FileHandler {
+    public:
+        FileHandler() = default;
 
-    static void writeFile(const std::filesystem::path &filePath, const std::vector<uint8_t> &data);
-};
+        ~FileHandler();
 
-#endif // FILEHANDLER_H
+        /**
+          * @brief Load a file into memory.
+          * @param filePath The path of the file to load.
+          * @param chunkSize The size of the chunks to load the file in.
+          * @return true if the file was successfully loaded, false otherwise.
+        */
+        bool loadFile(const std::string &filePath, size_t chunkSize = 4096);
+
+        /**
+          * @brief Save a string to a file.
+          * @param filePath The path of the file to save to.
+          * @param content The content to save to the file.
+          * @param chunkSize The size of the chunks to save the file in.
+          * @return true if the file was successfully saved, false otherwise.
+        */
+        bool saveFile(const std::string &filePath, const std::string &content, size_t chunkSize = 4096);
+
+        /**
+          * @brief Get the content of the loaded file.
+          * @return The content of the loaded file.
+        */
+        [[nodiscard]] std::string getFileContent() const;
+
+    private:
+        std::string fileContent;
+        std::mutex fileMutex;
+        int fileDescriptor = -1;
+        size_t fileSize = 0;
+
+        bool mapFilePart(off_t offset, size_t size, int prot, void *&mappedData) const;
+
+        bool loadFileChunk(off_t offset, size_t size);
+
+        [[nodiscard]] bool saveFileChunk(const std::string &content, off_t offset, size_t size) const;
+
+        static void unmapFile(void *mappedData, size_t size);
+
+        void closeFile();
+    };
+}
