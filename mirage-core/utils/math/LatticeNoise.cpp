eaad73a8314e0588dd494ae8b7d25cf0805b7524
sirlolcat <sirlolcatX@pm.me>
2024-05-31 17:43:45 +0200

    fix: adjust LatticeNoise class
    Signed-off-by: sirlolcat <sirlolcatX@pm.me>

--- mirage-core/utils/math/LatticeNoise.cpp
+++ mirage-core/utils/math/LatticeNoise.cpp
@@ -2,13 +2,23 @@
 #include <random>
 
 namespace utils::math {
-    void LatticeNoise::addLatticeNoise(std::vector<unsigned char> &data) {
+    void LatticeNoise::addLatticeNoise(std::vector<unsigned char> &data, size_t length) {
         std::default_random_engine generator(std::random_device{}());
         std::uniform_int_distribution distribution(0, MODULUS - 1);
 
-        for (auto &byte: data) {
+        for (size_t i = 0; i < length; ++i) {
             const int noise = distribution(generator);
-            byte = static_cast<unsigned char>((static_cast<int>(byte) + noise) % MODULUS);
+            data[i] = static_cast<unsigned char>((static_cast<int>(data[i]) + noise) % MODULUS);
+        }
+    }
+
+    void LatticeNoise::removeLatticeNoise(std::vector<unsigned char> &data, size_t length) {
+        std::default_random_engine generator(std::random_device{}());
+        std::uniform_int_distribution distribution(0, MODULUS - 1);
+
+        for (size_t i = 0; i < length; ++i) {
+            const int noise = distribution(generator);
+            data[i] = static_cast<unsigned char>((static_cast<int>(data[i]) - noise + MODULUS) % MODULUS);
         }
     }
 }
--- mirage-core/utils/math/LatticeNoise.h
+++ mirage-core/utils/math/LatticeNoise.h
@@ -14,8 +14,19 @@ namespace utils::math {
          * This method adds lattice noise to the provided data by XORing it with random bytes.
          *
          * @param data The data to which lattice noise will be added.
+         * @param length The length of the data.
          */
-        static void addLatticeNoise(std::vector<unsigned char> &data);
+        static void addLatticeNoise(std::vector<unsigned char> &data, size_t length);
+
+        /**
+         * @brief Removes lattice noise from the provided data.
+         *
+         * This method removes lattice noise from the provided data by reversing the XOR operation.
+         *
+         * @param data The data from which lattice noise will be removed.
+         * @param length The length of the data.
+         */
+        static void removeLatticeNoise(std::vector<unsigned char> &data, size_t length);
     };
 } // namespace utils::math
 
